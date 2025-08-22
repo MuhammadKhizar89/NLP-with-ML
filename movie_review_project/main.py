0b268cceb45e98e686b9f894f212fc6133d108c0
unknown <muhammadkhizar8919@gmail.com>
2025-08-23 00:05:15 +0500

    Deployment

--- movie_review_project/main.py
+++ movie_review_project/main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import re
 import string
 import nltk 
@@ -10,11 +11,11 @@ nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 stop_words=set(stopwords.words('english'))
+
 def remove_stopwords(text):
     word_tokens=word_tokenize(text)
     filtered_sentence=[w for w in word_tokens if not w in stop_words]
     return ' '.join(filtered_sentence)
-
 
 def remove_html_tags(text):
     return re.sub(r'<.*?>', '', text)
@@ -36,7 +37,19 @@ def predict_review(review):
     review = remove_numbers(review)
     review = review.lower()
     review = remove_stopwords(review)
-    result=pipeline.predict([review])
-    return "Positive" if result == 1 else "Negative"
+    result=pipeline.predict([review])[0]
+    return "😊 Positive" if result == 1 else "☹️ Negative"
 
-print(predict_review("I hate this movie"))
+
+st.title("🎬 Movie Review Sentiment Analysis ")
+st.markdown("""This app uses **Natural Language Processing (NLP)** to analyze movie reviews.  
+""")
+
+st.write("Enter a movie review below to see if it's **positive or negative**.")
+review_text = st.text_area("Write your review here:")
+if st.button("Predict"):
+    if review_text.strip() != "":
+        prediction = predict_review(review_text)
+        st.success(f"Prediction: {prediction}")
+    else:
+        st.warning("Please enter some text first.")
